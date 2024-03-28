a966dd018e6de926765e357b6b606e35adf6b604
JayasriTopuri <jayasritopuri@gmail.com>
2024-03-28 14:04:58 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,13 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('jayasritopuri@gmail.com','yjro ohoc fuhb vvlq')
+    msg=EmailMessage()
+    msg['From']='jayasritopuri@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
